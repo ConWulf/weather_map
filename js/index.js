0f2c0583ba53c3ef80c58a4eb4f5835e722ccbe3
Connor Wulf <connor.m.wulf@gmail.com>
2021-01-01 15:58:48 -0600

    formatting weather tab content

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     const overviewCard = $("#current");
     const tabContent = $(".content");
     const tabs = $(".links");
-    const navbar = $("#nav");
+    const fiveDay = $("#fiveDay");
     const mapOptions = {
         container: 'map',
         center: [-98.49, 29.42], // starting position [lng, lat]
@@ -15,27 +15,34 @@ $(document).ready(function() {
         style: 'mapbox://styles/mapbox/satellite-streets-v11'
     }
 
-    // mapboxgl.accessToken = mapboxKey;
-    // const map = new mapboxgl.Map(mapOptions);
-    //
-    // const geocodeOptions = {
-    //     accessToken: mapboxKey,
-    //     mapboxgl: mapboxgl,
-    //     marker: false,
-    //     collapsed: true
-    // }
-    //
-    // const geocoder = new MapboxGeocoder(geocodeOptions);
-    // document.getElementById("geocoder").appendChild(geocoder.onAdd(map));
-    //
-    // const marker = new mapboxgl.Marker()
-    //     .setLngLat([-98.49, 29.42])
-    //     .addTo(map);
+    mapboxgl.accessToken = mapboxKey;
+    const map = new mapboxgl.Map(mapOptions);
+
+    const geocodeOptions = {
+        accessToken: mapboxKey,
+        mapboxgl: mapboxgl,
+        marker: false,
+        collapsed: true
+    }
+
+    const geocoder = new MapboxGeocoder(geocodeOptions);
+    document.getElementById("geocoder").appendChild(geocoder.onAdd(map));
+
+    const marker = new mapboxgl.Marker()
+        .setLngLat([-98.49, 29.42])
+        .addTo(map);
 
     const convertTime = (unix) => {
         let milliseconds = unix * 1000;
         let dateObj = new Date(milliseconds);
         let options = {hour: "numeric", minute: "numeric", second: "numeric", timeZoneName: "short"}
+        return dateObj.toLocaleString("en-US", options);
+    }
+
+    const convertDate = (unix) => {
+        let milliseconds = unix * 1000;
+        let dateObj = new Date(milliseconds);
+        let options = {weekday: "long", month: "long", day: "numeric"}
         return dateObj.toLocaleString("en-US", options);
     }
 
@@ -56,7 +63,7 @@ $(document).ready(function() {
         }} = weatherObj;
         return `<div id="currentCard" class="relative p-3 rounded-lg bg-white mx-auto bg-opacity-60 max-w-2xl text-sm">
         <div class="flex flex-col">
-        <a href="#" class="p-1">LOCATION</a>
+        <h3 href="#" class="text-lg">Weather Today In Location</h3>
         <h3>as of ${convertTime(dt)}</h3>
             <div>
                 <p class="right-0 top-0 absolute"><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather icon" class="w-20"></p>
@@ -72,6 +79,33 @@ $(document).ready(function() {
     </div>`
     }
 
+    const renderFiveDay = (weatherObj) => {
+        let html = ''
+        weatherObj.daily.forEach(obj => {
+            const {dt,
+                temp: { max, min},
+                weather: [{description, icon}],
+                wind_deg,
+                wind_speed,
+                uvi,
+                humidity
+            } = obj;
+            html += `<div>
+                        <span>${convertDate(dt)}</span>
+                        <span>${max}/${min}</span>
+                        <span><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather icon"></span>
+                        <span>${description}</span>
+                        <span>${humidity}</span>
+                        <span>${uvi}</span>
+                        <span>${wind_speed}</span>
+                        <span>${wind_deg}</span>
+                    </div>`
+        })
+        return html;
+    }
+
+
+
     const getWeather = (lat, long) => {
         return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${weatherKey}&units=imperial`)
             .then(res => res.json());
@@ -80,6 +114,7 @@ $(document).ready(function() {
     getWeather(29.42, -98.49).then(data => {
         console.log(data);
         overviewCard.append(renderCurrentWeather(data));
+        fiveDay.append(renderFiveDay(data))
     });
 
     slider.on('click', function() {
